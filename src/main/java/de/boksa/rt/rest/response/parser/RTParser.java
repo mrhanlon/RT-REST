3db1999de43037638b77d3f4a68455bd88655192
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2013-10-01 10:30:06 -0500

    indentation

--- src/main/java/de/boksa/rt/rest/response/parser/RTParser.java
+++ src/main/java/de/boksa/rt/rest/response/parser/RTParser.java
@@ -14,7 +14,8 @@ import de.boksa.rt.rest.RTRESTResponse;
 public class RTParser {
 	private static RTParser SINGLETON = new RTParser();
 
-	private RTParser() {}
+	private RTParser() {
+	}
 
 	public static RTParser getInstance() {
 		return SINGLETON;
@@ -23,7 +24,9 @@ public class RTParser {
 	private static final String DELIMITER = "\n\n--\n\n";
 	private static final String DELIMITER_LINES = "\n";
 	private static final Pattern PATTERN_START_FIELD = Pattern.compile("^(\\w+?): ?(.*)");
-	//private static final Pattern CUSTOM_FIELD = Pattern.compile("^CF.\\{(.*)\\}: ?(.*)"); TODO this should be implemented at some point
+	// private static final Pattern CUSTOM_FIELD =
+	// Pattern.compile("^CF.\\{(.*)\\}: ?(.*)"); TODO this should be implemented
+	// at some point
 	private static final Pattern COMMENT_LINE = Pattern.compile("^#.*");
 
 	public List<Map<String, String>> parseResponse(RTRESTResponse response) {
@@ -33,45 +36,44 @@ public class RTParser {
 			return processSingleLine(response.getBody());
 		}
 	}
-	
+
 	private List<Map<String, String>> processMultiLine(String responseBody) {
-		List<Map<String,String>> resultData = new LinkedList<Map<String,String>>();
-		for (String responseString : responseBody.split(DELIMITER)) {	
+		List<Map<String, String>> resultData = new LinkedList<Map<String, String>>();
+		for (String responseString : responseBody.split(DELIMITER)) {
 			resultData.addAll(processSingleLine(responseString));
 		}
-		
+
 		return resultData;
 	}
-	
+
 	private List<Map<String, String>> processSingleLine(String responseBody) {
 
-			Map<String,String> responseData = new HashMap<String,String>();
-			List<Map<String,String>> resultData = new LinkedList<Map<String,String>>();
+		Map<String, String> responseData = new HashMap<String, String>();
+		List<Map<String, String>> resultData = new LinkedList<Map<String, String>>();
 
-			String fieldName = null;
-			StringBuffer tmp = new StringBuffer();
-			for (String responseLine : responseBody.split(DELIMITER_LINES)) {
-				Matcher m = PATTERN_START_FIELD.matcher(responseLine);
-				if (m.matches()) {
-					if (fieldName != null) {
-						if (COMMENT_LINE.matcher(tmp.toString()).matches()) {
-							String id = tmp.toString().replaceAll("#.*\\(.*\\)", "");
-							tmp.setLength(0);
-							tmp.append(id);
-						}
-						responseData.put(WordUtils.uncapitalize(fieldName), tmp.toString().replaceFirst("<br />$", ""));
+		String fieldName = null;
+		StringBuffer tmp = new StringBuffer();
+		for (String responseLine : responseBody.split(DELIMITER_LINES)) {
+			Matcher m = PATTERN_START_FIELD.matcher(responseLine);
+			if (m.matches()) {
+				if (fieldName != null) {
+					if (COMMENT_LINE.matcher(tmp.toString()).matches()) {
+						String id = tmp.toString().replaceAll("#.*\\(.*\\)", "");
 						tmp.setLength(0);
+						tmp.append(id);
 					}
-					
-					fieldName = m.group(1);
-					tmp.append(m.group(2));
-				} else {
-					tmp.append(responseLine + "<br />");
+					responseData.put(WordUtils.uncapitalize(fieldName), tmp.toString().replaceFirst("<br />$", ""));
+					tmp.setLength(0);
 				}
-			}	
-			responseData.put(WordUtils.uncapitalize(fieldName), tmp.toString());
-			resultData.add(responseData);
-			return resultData;
+
+				fieldName = m.group(1);
+				tmp.append(m.group(2));
+			} else {
+				tmp.append(responseLine + "<br />");
+			}
+		}
+		responseData.put(WordUtils.uncapitalize(fieldName), tmp.toString());
+		resultData.add(responseData);
+		return resultData;
 	}
 }
-
